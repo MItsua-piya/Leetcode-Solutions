32951463cf6085498d95acaadddaa805764c5e7e
PriyaWankhade <priyawankhade0314@gmail.com>
2026-07-09 23:07:27 +0530

    Time: 0 ms (100.00%) | Memory: 25.6 MB (83.93%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        int n=grid.size();
-       int N=n*n;
-        vector<int>freq(N+1,0);
-         for (auto &row : grid) {
+
+        long long n = grid.size();
+        long long N = n * n;
+
+        long long expectedSum = N * (N + 1) / 2;
+        long long expectedSq = N * (N + 1) * (2 * N + 1) / 6;
+
+        long long actualSum = 0;
+        long long actualSq = 0;
+
+        for (auto &row : grid) {
             for (int x : row) {
-                freq[x]++;
+                actualSum += x;
+                actualSq += 1LL * x * x;
             }
         }
-        int repeated = -1, missing = -1;
 
-        for (int i = 1; i <= N; i++) {
-            if (freq[i] == 2)
-                repeated = i;
-            else if (freq[i] == 0)
-                missing = i;
-        }
+        long long A = actualSum - expectedSum;          // x - y
+        long long B = (actualSq - expectedSq) / A;      // x + y
 
-        return {repeated, missing};
+        long long repeated = (A + B) / 2;
+        long long missing = repeated - A;
+
+        return {(int)repeated, (int)missing};
     }
 };
