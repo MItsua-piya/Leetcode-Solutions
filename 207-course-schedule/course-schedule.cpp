5253f3579e777f3a813396bd7c067690b433cde5
PriyaWankhade <priyawankhade0314@gmail.com>
2026-07-15 17:35:20 +0530

    Time: 4 ms (63.75%) | Memory: 20.2 MB (16.92%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,54 +1,54 @@
 class Graph{
-    int v;
-    list<int>*l;
-    public:
-    Graph(int v){
-        this->v=v;
-        l=new list<int>[v];
-    }
-    void addEdge(int u,int v){
-        l[u].push_back(v);
-        
-    }
-
-bool helper(int src ,vector<bool>&vis,vector<bool>&recPath){
+int v;
+list<int>*l;
+public:
+Graph(int v){
+    this->v=v;
+    l=new list<int>[v];
+}
+void addEdge(int u,int v){
+    l[u].push_back(v);
+}
+bool detect(int src,int par,vector<bool>&vis,vector<bool>&rec){
 vis[src]=true;
-recPath[src]=true;
-for(auto v:l[src]){
-    if(!vis[v]){
-        if(helper(v,vis,recPath)){
-            return true;
-        }
+rec[src]=true;
+for(auto s:l[src]){
+    if(!vis[s]){
+        if (detect(s, src, vis, rec))
+        return true;
     }else{
-        if(recPath[v]){
+        if(rec[s]){
             return true;
         }
     }
+  
 }
-recPath[src]=false;
-return false;
-}
-    bool dfs(){
-     vector<bool>vis(v,false);
-     vector<bool>recPath(v,false);
-     for(int i=0;i<v;i++){
+rec[src]=false;
+
+  return false;
+};
+bool dfs(){
+    vector<bool>vis(v,false);
+   vector<bool>rec(v,false);
+    for(int i=0;i<v;i++){
         if(!vis[i]){
-            if(helper(i,vis,recPath))
-            return true;
+            if(detect(i,-1,vis,rec)){
+                return true;
+            };
         }
-     }
-     return false;
     }
+    return false;
+}
 };
 
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         Graph g(numCourses);
-        for(auto &p :prerequisites){
-            g.addEdge(p[1],p[0]);
-        }
-        if( g.dfs()) return false;
-        return true;
+        for(auto p:prerequisites){
+        
+                g.addEdge(p[0],p[1]);}
+           
+        return !g.dfs();
     }
 };
