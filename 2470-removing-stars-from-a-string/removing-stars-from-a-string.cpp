1217d8b00fa47785f5931ec8a2a419e26c3fa68c
PriyaWankhade <priyawankhade0314@gmail.com>
2026-07-25 18:51:35 +0530

    Time: 22 ms (38.14%) | Memory: 28.5 MB (53.36%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string ans="";
+        ans+=s[0];
+        for(int i=1;i<s.length();i++){
+         if(s[i]=='*'){
+            ans.pop_back();
+         }else{
+            ans.push_back(s[i]);
+         }
+        }
+        return ans;
+    }
+};
