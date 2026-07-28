1e4fa7db7fa66dcf2098f7100e83d5fbcc6cebb8
PriyaWankhade <priyawankhade0314@gmail.com>
2026-07-28 08:59:16 +0530

    Time: 928 ms (7.04%) | Memory: 11.8 MB (79.73%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,34 +1,15 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int offset;
 
-    int solve(vector<int>& nums, int i, int sum, int target) {
-        if (i == nums.size()) {
-            return (sum == target) ? 1 : 0;
-        }
-
-        if (dp[i][sum + offset] != -1)
-            return dp[i][sum + offset];
-
-        int add = solve(nums, i + 1, sum + nums[i], target);
-        int subtract = solve(nums, i + 1, sum - nums[i], target);
-
-        return dp[i][sum + offset] = add + subtract;
-    }
-
+int help(vector<int>& nums, int target,int sum,int idx){
+if(idx == nums.size()){
+    return sum == target;
+}
+    int plus=help(nums,target,sum+nums[idx],idx+1);
+       int minus=help(nums,target,sum-nums[idx],idx+1);
+       return plus+minus;
+}
     int findTargetSumWays(vector<int>& nums, int target) {
-        int total = 0;
-        for (int num : nums)
-            total += num;
-
-        // If target is outside the possible range
-        if (abs(target) > total)
-            return 0;
-
-        offset = total;
-        dp.assign(nums.size(), vector<int>(2 * total + 1, -1));
-
-        return solve(nums, 0, 0, target);
+        return help(nums,target,0,0);
     }
 };
